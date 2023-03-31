1f9a1c64b11a0373393fb38455548cfef9782b8d
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-03-31 12:15:22 +0530

    Updates

---
+++ precache-manifest.91505760a7550a097511d0a7f535f93b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "edc50a97c75d1ed85412",
+    "url": "/my-app/static/css/main.79920cf2.chunk.css"
+  },
+  {
+    "revision": "edc50a97c75d1ed85412",
+    "url": "/my-app/static/js/main.edc50a97.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/my-app/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "837534ddb3fa8e994385",
+    "url": "/my-app/static/css/2.009eb654.chunk.css"
+  },
+  {
+    "revision": "837534ddb3fa8e994385",
+    "url": "/my-app/static/js/2.837534dd.chunk.js"
+  },
+  {
+    "revision": "2a0e135474bb9271673e",
+    "url": "/my-app/static/js/runtime~main.2a0e1354.js"
+  },
+  {
+    "revision": "22bbcc5a382f29f8c0dd683faef0e280",
+    "url": "/my-app/index.html"
+  }
+];
