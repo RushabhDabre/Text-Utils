1c85b5c19a146e9af6f4d23739643ed458f75bd7
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-03-31 19:35:35 +0530

    improve UI

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -51,10 +51,10 @@ export default function TextForm(props) {
                     <textarea className="form-control" value={text} onChange={handleOnClick} style={{backgroundColor: props.mode === 'dark' ? 'black' : 'white', color: props.mode === 'dark' ? 'white' : 'black'}} id="myBox" rows="8" ></textarea>
                 </div>
 
-                <button className={`btn btn-${props.mode} mx-1`} onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className={`btn btn-${props.mode} mx-1`} onClick={handleLoClick}>Convert to Lowercase</button>
-                <button className={`btn btn-${props.mode} mx-1`} onClick={handleClearClick}>Clear the text</button>
-                <button className={`btn btn-${props.mode} mx-1`} onClick={handleCopy}>Copy to clipboard</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleClearClick}>Clear the text</button>
+                <button className={`btn btn-${props.mode} mx-1 my-1`} onClick={handleCopy}>Copy to clipboard</button>
 
                 {/* <button className={`btn btn-${props.mode==='light'? 'dark':'light'} mx-1`} onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className={`btn btn-${props.mode==='light'? 'dark':'light'} mx-1`} onClick={handleLoClick}>Convert to Lowercase</button>
@@ -65,7 +65,8 @@ export default function TextForm(props) {
             </div>
             <div className='container my-3' style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
                 <h2>Yor text Summary</h2>
-                <p>{text.split(" ").filter(function(text){return text!=="";}).length} Words {text.length} Characters</p>
+                {/* <p>{text.split(" ").filter(function(text){return text!=="";}).length} Words {text.length} Characters</p> */}
+                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} Words {text.length} Characters</p>
                 <p>{0.008 * text.split(" ").length} Minutes read</p>
                 <h2>Yor text Summary</h2>
                 <p>{text.length>0?text:"Enter something in the textbox above for preview"}</p>
